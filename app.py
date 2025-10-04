c3a2238bcd6e069032bfa5b6fd85948bd544ccd9
Shaik Dadu <dadushaik185@gmail.com>
2025-10-04 20:15:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,39 @@
-from flask import Flask, render_template, request
+#importing required libraries
+
+from flask import Flask, request, render_template
+import numpy as np
 import pandas as pd
-from pycaret.classification import load_model, predict_model
-import featureExtractor
+from sklearn import metrics 
+import warnings
+import pickle
+warnings.filterwarnings('ignore')
+from feature import FeatureExtraction
+
+file = open("pickle/model.pkl","rb")
+gbc = pickle.load(file)
+file.close()
+
 
 app = Flask(__name__)
-model = load_model('phishingdetection')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        url = request.form['url']
-        input_df = pd.DataFrame([{'url': url}])
-        
-        result = predict_model(model, data=input_df)
-        print(result)  # Debug: See what columns are there
-        
-        prediction = result['Label'][0]  # Use correct key
-        return render_template('index.html', prediction=prediction)
+        url = request.form["url"]
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1,30) 
+
+        y_pred =gbc.predict(x)[0]
+        #1 is safe       
+        #-1 is unsafe
+        y_pro_phishing = gbc.predict_proba(x)[0,0]
+        y_pro_non_phishing = gbc.predict_proba(x)[0,1]
+        # if(y_pred ==1 ):
+        pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
+        return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
+    return render_template("index.html", xx =-1)
+
 
 if __name__ == "__main__":
-    print("Starting Flask server...")
     app.run(debug=True)
-
-
-
-# app.py
-
-# from flask import Flask, render_template, request
-# import pandas as pd
-# from pycaret.classification import load_model
-# import featureExtractor  # our custom feature extractor
-
-# app = Flask(__name__)
-
-# # Load PyCaret model
-# model = load_model('phishingdetection')  # load PyCaret model
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     url = request.form['url']
-#     features = featureExtractor.extract_features(url)
-#     print(f"URL: {url}, Extracted features: {features}")  # Debug
-#     df = pd.DataFrame([features], columns=['url_length', 'dot_count', 'slash_count', 'contains_https'])
-#     prediction = model.predict(df)
-#     print(f"Prediction: {prediction[0]}")  # Debug
-#     return str(prediction[0])
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-  
